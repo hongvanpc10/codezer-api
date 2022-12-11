ae2e8a4d4df92623f0f1b32421fd6211b9acb1f5
hongvanpc10 <hongvanpc10@gmail.com>
2022-12-11 14:11:38 +0700

    ervgrg

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,4 +2,3 @@ export * as authController from './auth.controller'
 export * as blogsController from './blogs.controller'
 export * as categoriesController from './categories.controller'
 export * as usersController from './users.controller'
-
